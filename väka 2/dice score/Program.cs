daf79db9688bfa3b444dba67a27a3a11b9005c96
gabbe01 <71494202+gabbe01@users.noreply.github.com>
2020-09-22 17:09:25 +0200

    complet dice score

---
+++ väka 2/dice score/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace dice_score
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var random = new Random();
+            int diceTrow = random.Next(1, 7);
+            int diceTrow2 = random.Next(1, 7);
+            int diceTrow3 = random.Next(1, 7); 
+            int sum = diceTrow + diceTrow2 + diceTrow3 *3;
+            int sum2 = sum * 2;
+            Console.WriteLine("press enter to ROLL....");
+            Console.ReadKey(true);
+            Console.WriteLine($"first ROLL is: {diceTrow}");
+            Console.ReadKey(true);
+            Console.WriteLine($"second ROLL is: {diceTrow2}");
+            Console.ReadKey(true);
+            Console.WriteLine($"third ROLL is: {diceTrow3}");
+            
+            Console.WriteLine("press enter for score");
+            Console.ReadKey(true);
+            Console.WriteLine($"total score is: {sum2}");
+            
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
