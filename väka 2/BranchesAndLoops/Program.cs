efaad2c51d43905b1e0c1b09aebc154264e9f183
gabbe01 <71494202+gabbe01@users.noreply.github.com>
2020-09-22 15:37:50 +0200

    complet sum

---
+++ väka 2/BranchesAndLoops/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+
+namespace BranchesAndLoops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum;
+            
+            sum = 0;
+
+            for (int i = 1; i <= 20; i++)
+            {
+                if (i % 3 == 0)
+                {
+                    sum = sum + i;
+                } 
+                
+
+
+
+            }
+            Console.WriteLine($"the sum is {sum}"); 
+
+
+
+        }       
+    }
+}
