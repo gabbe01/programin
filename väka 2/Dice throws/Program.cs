1c655f466e087a9a4a4bebbf78e2daccbe67e4a6
gabbe01 <71494202+gabbe01@users.noreply.github.com>
2020-09-22 15:38:33 +0200

    complet dice throws

---
+++ väka 2/Dice throws/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Dice_throws
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   var random = new Random(); 
+            int diceThrow = random.Next(1, 7);
+            int diceThrow2 = random.Next(1, 7);
+            int diceThrow3 = random.Next(1, 7);
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine($"first dice throw is:{diceThrow}");
+            Console.WriteLine($"second dice throw is:{diceThrow2}");
+            Console.WriteLine($"third dice throw is:{diceThrow3}");
+            int totalsum = diceThrow + diceThrow2 + diceThrow3;
+            Console.WriteLine($"SCORE:{totalsum}"); 
+            
+        }
+    }
+}
