032ceea004ea7dd7dd8b470ddfc597f6352490d2
gabbe01 <71494202+gabbe01@users.noreply.github.com>
2020-09-22 15:40:38 +0200

    complet regenerate

--- väka 2/Regenerate/Program.cs
+++ väka 2/Regenerate/Program.cs
@@ -1,12 +1,29 @@
 ﻿using System;
+using System.Net.Sockets;
 
-namespace Regenerate
+namespace Regenerate 
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var random = new Random();
+            int warriorhp = random.Next(1, 101);
+            Console.WriteLine($"Warrior HP:{warriorhp}");
+            Console.WriteLine($"Regenerate spell is cast!");
+
+            while (warriorhp < 50)
+            {
+                warriorhp = warriorhp + 10;
+                Console.WriteLine($"warrior hp:{warriorhp}");
+            }
+
+            Console.WriteLine($"Regenerate spell is complete");
+
+            
+
+
+
         }
     }
 }
