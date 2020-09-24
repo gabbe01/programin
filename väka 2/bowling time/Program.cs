c4f0c00db085fc824fd41716e72da226aff5e3ad
gabbe01 <71494202+gabbe01@users.noreply.github.com>
2020-09-24 12:59:22 +0200

    complet bowling

---
+++ väka 2/bowling time/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace bowling_time
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var random = new Random();
+
+            int pinsKnocked1stThrow = random.Next(0, 11);
+            int pinsKnocked2ndThrow = random.Next(0, 11 - pinsKnocked1stThrow);
+
+            Console.WriteLine($"First roll: {pinsKnocked1stThrow}");
+            Console.WriteLine($"Second roll: {pinsKnocked2ndThrow}");
+        }
+    }
+}
