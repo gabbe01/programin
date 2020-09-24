97013ace7b80109d93692b2557ab73c59753a639
gabbe01 <71494202+gabbe01@users.noreply.github.com>
2020-09-24 15:04:06 +0200

    complet alpha centauri

---
+++ väka 2/alpha centauri/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace alpha_centauri
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            double oneLightYearInParsecs = 0.306594845; 
+            double distanceInLightYears = 4.365;
+            double distanceInParsecs = distanceInLightYears * oneLightYearInParsecs;
+            Console.WriteLine(distanceInParsecs);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
