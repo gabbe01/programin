848ffc0b5d73870141c33ff69592dabb591c29a8
gabbe01 <71494202+gabbe01@users.noreply.github.com>
2020-09-24 13:57:40 +0200

    complet hit accuracy

---
+++ väka 2/Hit accuracy/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Xml.Schema;
+
+namespace Hit_accuracy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var random = new Random();
+            int totalShots = random.Next(10, 21);
+            int hits = random.Next(0, totalShots+1);
+            Console.WriteLine($"Total shots: {totalShots}");
+            Console.WriteLine($"Hits made: {hits}");
+            
+
+            double hitAccuracy = (double)hits / (double)totalShots;
+            double hitAccuracyPercentage = (100 * hitAccuracy);
+            Console.WriteLine($"Hit accuracy: {hitAccuracyPercentage}%");
+
+        }
+    }
+}
