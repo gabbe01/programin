bc44247138273c82e727408b885f9fa11222043d
gabbe01 <71494202+gabbe01@users.noreply.github.com>
2020-09-28 22:56:25 +0200

    complet bowling time

--- väka 2/bowling time/Program.cs
+++ väka 2/bowling time/Program.cs
@@ -11,9 +11,28 @@ namespace bowling_time
 
             int pinsKnocked1stThrow = random.Next(0, 11);
             int pinsKnocked2ndThrow = random.Next(0, 11 - pinsKnocked1stThrow);
-
-            Console.WriteLine($"First roll: {pinsKnocked1stThrow}");
-            Console.WriteLine($"Second roll: {pinsKnocked2ndThrow}");
+            int totalpinsknocked = pinsKnocked1stThrow + pinsKnocked2ndThrow; 
+            if (pinsKnocked1stThrow ==  10)
+            {
+                Console.WriteLine("First roll: X ");
+            }
+            else
+            {
+                Console.WriteLine($"First roll: {pinsKnocked1stThrow}");
+            }
+            if (pinsKnocked1stThrow == 10)
+            {
+                Console.Write("");
+            }
+            else if (totalpinsknocked == 10 && pinsKnocked1stThrow < 10) 
+            {
+                Console.WriteLine("Second roll: / ");
+            }
+            else
+            {
+                Console.WriteLine($"Second roll: {pinsKnocked2ndThrow}");
+            }
+            Console.WriteLine($"Knocked pins: {totalpinsknocked}");
         }
     }
 }
