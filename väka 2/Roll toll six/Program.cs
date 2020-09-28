ae03e588a55dafa020c197bd20a2ae82035aa569
gabbe01 <71494202+gabbe01@users.noreply.github.com>
2020-09-28 22:55:31 +0200

    compel dice six

---
+++ väka 2/Roll toll six/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Roll_toll_six
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0; 
+            var random = new Random();
+            int diceTrow = random.Next(1, 7);
+            if (diceTrow == 6)
+            {
+                Console.WriteLine($"The player rolls: {diceTrow}");
+                sum = sum + diceTrow;
+            }
+
+            while (diceTrow < 6)
+            {
+                diceTrow = random.Next(1, 7);
+                Console.WriteLine($"The player rolls: {diceTrow}");
+                sum = sum + diceTrow;
+               
+            }
+            Console.WriteLine($"Total score: {sum}");
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}       
