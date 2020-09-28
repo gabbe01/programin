075c9f6a3bd893f030a28c121f5c3eec42b6dd6c
gabbe01 <71494202+gabbe01@users.noreply.github.com>
2020-09-28 22:56:49 +0200

    comblet bowling score

---
+++ week 4/Bowling score sheet/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Bowling_score_sheet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int totalFrames = 5;
+
+
+
+
+            //+-----_+-----_+
+            for (int i = 0; i < totalFrames; i++) 
+            {
+                Console.Write("+-----");
+            }
+            Console.WriteLine ("+");
+
+            //| | | _| | | _|
+            for (int i = 0; i < totalFrames; i++)
+            {
+                Console.Write("| | | ");
+            }
+            Console.WriteLine("|");
+
+
+            //| ----_| ----_|
+            for (int i = 0; i < totalFrames; i++)
+            {
+                Console.Write("| ----");
+            }
+            Console.WriteLine("|");
+
+
+            //|     _|     _|
+            for (int i = 0; i < totalFrames; i++)
+            {
+                Console.Write("|     ");
+            }
+            Console.WriteLine("|");
+
+            //+-----_+-----_+
+            for (int i = 0; i < totalFrames; i++)
+            {
+                Console.Write("+-----");
+            }
+            Console.WriteLine("+");
+        }
+    }
+}
