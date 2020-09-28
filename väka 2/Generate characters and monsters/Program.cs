f46ae769e935c9f10f042cbc0c2b5e3b9761d92b
gabbe01 <71494202+gabbe01@users.noreply.github.com>
2020-09-28 22:54:29 +0200

    complet generate

---
+++ väka 2/Generate characters and monsters/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Generate_characters_and_monsters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var random = new Random();
+
+            int Hp = 40;
+            int Hparmy = 40;
+            int str = 0;
+            for (int i = 0; i < 3; i++) 
+            {
+                int D6 = random.Next(1, 7);
+                str = str + D6;
+            }
+            Console.WriteLine($"Gabriel was born with only {str} strength....");
+
+            for (int i = 0; i < 8; i++)
+            {
+                int D10 = random.Next(1, 11);
+                Hp = Hp + D10;
+            }
+            Console.WriteLine($"Retronator appears from the shadow with a massive sword and a whopping {Hp} hp!");
+            Console.WriteLine("Oh shit....");
+
+            for (int j = 0; j < 100; j++)
+            {
+                for (int i = 0; i < 8; i++)
+                {
+                    int D10 = random.Next(1, 11);
+                    Hparmy = Hparmy + D10;
+                }
+            }
+            Console.WriteLine($"100 of Retronators questers with {Hparmy} hp joins the battle!");
+            Console.WriteLine("Oh shit....");
+        }
+    }
+}
