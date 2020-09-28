9c8364ce090a5dbf57a71e21076f8744777dc420
gabbe01 <71494202+gabbe01@users.noreply.github.com>
2020-09-28 22:57:37 +0200

    started tank battel

---
+++ TANK/MEGA TANK BATTLE/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace MEGA_TANK_BATTLE
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var random = new Random();
+            int tankDistance = random.Next(40, 71);
+            int width = 80;
+            
+            Console.WriteLine("DANGER! A tank is approaching our position. Your artilery unit is our only hope!");
+            Console.WriteLine("");
+            Console.WriteLine("Here is the map of the battlefield:");
+            
+            Console.ReadKey();
+        }    
+    }
+}
