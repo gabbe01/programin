9d1dcb18485ae8d7c58368cd57cb3a44b2792a92
gabbe01 <71494202+gabbe01@users.noreply.github.com>
2020-09-29 14:56:24 +0200

    complet bowling score 2

---
+++ week 4/bowling score part 2/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace bowling_score_part_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            
+                int totalFrames = 5;
+            for (int i = 0; i < totalFrames; i++)
+            {
+                Console.Write("+-----");
+            }
+            Console.WriteLine("+");
+
+            //| | | _| | | _|
+            for (int i = 0; i < totalFrames; i++)
+            {
+                Console.Write("| | | ");
+            }
+            Console.WriteLine("|");
+
+
+            //| ----_| ----_|
+            for (int i = 0; i < totalFrames; i++)
+            {
+                Console.Write("| ----");
+            }
+            Console.WriteLine("|");
+
+
+            //|     _|     _|
+            for (int i = 0; i < totalFrames; i++)
+            {
+                Console.Write("|     ");
+            }
+            Console.WriteLine("|");
+
+            //+-----_+-----_+
+            for (int i = 0; i < totalFrames; i++)
+            {
+                Console.Write("+-----");
+            }
+            Console.WriteLine("+");
+        }
+    }    
+}
