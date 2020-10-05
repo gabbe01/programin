2ca2f09d9590e0bbb1f900f182dfca2beddec04e
gabbe01 <71494202+gabbe01@users.noreply.github.com>
2020-10-05 16:48:25 +0200

    mega tank battle 1

--- TANK/MEGA TANK BATTLE/Program.cs
+++ TANK/MEGA TANK BATTLE/Program.cs
@@ -4,17 +4,42 @@ namespace MEGA_TANK_BATTLE
 {
     class Program
     {
+        private static int width;
+
         static void Main(string[] args)
         {
             var random = new Random();
-            int tankDistance = random.Next(40, 71);
-            int width = 80;
+            int tankDistance = random.Next(40, 70);
             
-            Console.WriteLine("DANGER! A tank is approaching our position. Your artilery unit is our only hope!");
-            Console.WriteLine("");
+            Console.WriteLine("DANGER! A tank is approaching our position. help us obi wan kenobi Your our only hope!");
+            Console.WriteLine();
             Console.WriteLine("Here is the map of the battlefield:");
-            
-            Console.ReadKey();
-        }    
+            Console.WriteLine();
+
+            // Draw the artillery (2 characters)
+            Console.Write($"_/");
+
+            // Draw the ground between artillery to tank (tankDistance characters)
+            for (int i = 0; i < tankDistance; i++)
+            {
+                Console.Write($"_");
+            }
+
+            // Draw the tank (1 character)
+            Console.Write($"T");
+
+            // Draw the rest of the ground (restOfGrondDistance characters ... so that total is 80 characters)
+            int restOfGrondDistance = 80 - 1 - 2 - tankDistance;
+            for (int i = 0; i < restOfGrondDistance; i++)
+            {
+                Console.Write($"_");
+            }
+            Console.WriteLine();
+
+
+
+
+
+        }
     }
 }
--- week 4/Bowling score sheet/Program.cs
+++ week 4/Bowling score sheet/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Security.Cryptography.X509Certificates;
 
 namespace Bowling_score_sheet
 {
@@ -6,22 +7,60 @@ namespace Bowling_score_sheet
     {
         static void Main(string[] args)
         {
-            int totalFrames = 5;
-
+            int totalFrames = 10;
+            var random = new Random();
 
 
 
             //+-----_+-----_+
-            for (int i = 0; i < totalFrames; i++) 
+            for (int i = 0; i < totalFrames; i++)
             {
                 Console.Write("+-----");
             }
-            Console.WriteLine ("+");
+            Console.WriteLine("+");
 
             //| | | _| | | _|
             for (int i = 0; i < totalFrames; i++)
             {
-                Console.Write("| | | ");
+                // Bowling frame simulation code.
+                int pinsKnocked1stThrow = random.Next(0, 11);
+                int pinsKnocked2ndThrow = random.Next(0, 11 - pinsKnocked1stThrow);
+                int totalpinsknocked = pinsKnocked1stThrow + pinsKnocked2ndThrow;
+                
+                string roll1Text = $"{pinsKnocked1stThrow}";
+                string roll2Text = $"{pinsKnocked2ndThrow}";
+
+                // Check if the frame is a strike.
+                if (pinsKnocked1stThrow == 10)
+                {
+                    // Change the text to show a strike.
+                    roll1Text = $"X";
+                    roll2Text = $" ";
+                }
+                // Check if frame was a spare.
+                else if (totalpinsknocked == 10)
+                {
+                    // Change text to show a spare.
+                    roll2Text = $"/";
+                }
+                 
+                // Check if first throw was a 0.
+                if (pinsKnocked1stThrow == 0)
+                {
+                    roll1Text = $"-";
+                }
+
+
+                // Check if second throw was a 0.
+                if (pinsKnocked2ndThrow == 0)
+                {
+                    roll2Text = $"-";
+                }
+
+
+
+                // Write the result of frame.
+                Console.Write($"| |{roll1Text}|{roll2Text}");
             }
             Console.WriteLine("|");
 
