8154e7858bad6d5ad40dbe8edd6f8d66a71e44df
gabbe01 <71494202+gabbe01@users.noreply.github.com>
2020-11-06 10:35:34 +0100

    4 i rad

---
+++ fyra i rad/fyra i rad/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+
+namespace fyra_i_rad
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            int boardHeight = 6;
+            int boardWidth = 7;
+
+            // line 1
+            Console.Write(" ");
+            for (int i = 1; i <= boardWidth; i++)
+            {
+                Console.Write($" {i}");
+            }
+            Console.WriteLine();
+
+            // line 2
+            Console.Write(" ");
+            int numberOfUnderscores = 2 * boardWidth + 1;
+            for (int i = 0; i < numberOfUnderscores; i++)
+            {
+                Console.Write("_");
+            }
+            Console.WriteLine();
+
+            // line 3
+            Console.Write("|");
+            for (int i = 0; i < numberOfUnderscores; i++)
+            {
+                Console.Write(" ");
+            }
+            Console.WriteLine("|");
+
+
+            // line 4
+            for (int i = 0; i < boardHeight; i++)
+            {
+                Console.Write("|");
+                for (int j = 0; j < boardWidth; j++)
+                {
+                    Console.Write(" O");
+                }
+                Console.WriteLine(" |");
+            }
+
+
+            // line 5 
+            Console.Write("|");
+            for (int i = 0; i <numberOfUnderscores; i++)
+            {
+                Console.Write("_");
+            }
+            Console.WriteLine("|");
+
+            //line 6
+            Console.Write("|");
+            for (int i = 0; i < numberOfUnderscores; i++)
+            {
+                Console.Write("-");
+            }
+            Console.WriteLine("|");
+
+
+            // line 7
+            Console.Write("\\");
+            for (int i = 0; i < numberOfUnderscores; i++)
+            {
+                Console.Write("_");
+            }
+            Console.WriteLine("/");
+
+
+
+
+        }   
+    }
+}
