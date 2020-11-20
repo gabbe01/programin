3134bf23c233afbf693fba7053c0de09ae1676f3
gabbe01 <71494202+gabbe01@users.noreply.github.com>
2020-11-20 15:37:01 +0100

    färdig hello player

---
+++ week 2/day 1/HelloPlayer/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace HelloPlayer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string warrior = Console.ReadLine();
+            Console.Write($"Welcome to Dungeon World!. what's your name now again i forgot? how i just told you. im old i cant help it. like i told you before my name is {warrior} ");
+           
+        }
+    }
+}
